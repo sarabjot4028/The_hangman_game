f075c6fd16c640e8315e79800c0fce0b748188fb
sarabjot4028 <khelasrb@gmail.com>
2024-04-09 22:39:15 +0530

    this is tthe first commit

---
+++ hangman.py
@@ -0,0 +1,128 @@
+import random
+
+def choose_word():
+    words = ["hangman", "python", "computer", "program", "language", "game", "code"]
+    return random.choice(words)
+
+def display_word(word, guessed_letters):
+    displayed_word = ""
+    for letter in word:
+        if letter in guessed_letters:
+            displayed_word += letter
+        else:
+            displayed_word += "_"
+    return displayed_word
+
+def draw_hangman(attempts):
+    stages = [
+        """
+           _______
+          |/      |
+          |     
+          |     
+          |     
+          |   
+         _|___""",
+        """
+           _______
+          |/      |
+          |      (_)
+          |     
+          |     
+          |   
+         _|___""",
+        """
+           _______
+          |/      |
+          |      (_)
+          |       |
+          |       |
+          |   
+         _|___""",
+        """
+           _______
+          |/      |
+          |      (_)
+          |      /|
+          |       |
+          |   
+         _|___""",
+        """
+           _______
+          |/      |
+          |      (_)
+          |      /|\\
+          |       |
+          |   
+         _|___""",
+        """
+           _______
+          |/      |
+          |      (_)
+          |      /|\\
+          |       |
+          |      /
+         _|___""",
+        """
+           _______
+          |/      |
+          |      (_)
+          |      /|\\
+          |       |
+          |      / \\
+         _|___"""
+    ]
+    return stages[attempts]
+
+def word_relationship(word):
+    relationships = {
+        'python': 'a programming language',
+        'hangman': 'a guessing game',
+        'game': 'a form of entertainment',
+        'programming': 'the act of writing code',
+        'computer': 'a machine that performs computations',
+        'science': 'the study of the natural world'
+    }
+    return relationships.get(word, 'something interesting')
+
+def hangman():
+    word_to_guess = choose_word()
+    guessed_letters = []
+    attempts = 6
+    clue_given = False
+
+    print("Welcome to Hangman!")
+    print("Try to guess the word.")
+    print("You have 6 attempts to guess the word correctly.")
+    print("Here's a clue about the word: " + word_relationship(word_to_guess))
+
+    while attempts > 0:
+        print(draw_hangman(6 - attempts))
+        print(display_word(word_to_guess, guessed_letters))
+        guess = input("Guess a letter: ").lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Please enter a single letter.")
+            continue
+
+        if guess in guessed_letters:
+            print("You already guessed that letter.")
+            continue
+
+        guessed_letters.append(guess)
+
+        if guess not in word_to_guess:
+            attempts -= 1
+            print(f"Incorrect guess! You have {attempts} attempts left.")
+            if attempts == 0:
+                print("Sorry, you lost. The word was:", word_to_guess)
+                break
+        else:
+            print("Correct guess!")
+
+        if all(letter in guessed_letters for letter in word_to_guess):
+            print("Congratulations, you won!")
+            break
+
+if __name__ == "__main__":
+    hangman()
